77a5e01dc3a518647b58fe16ba0e658b33758842
Junyuan Zheng <joseph.zjy@gmail.com>
2017-05-27 15:57:14 -0700

    Add AddTwoNumber
    Add AddTwoNumber

---
+++ src/medium/AddTwoNumbers.java
@@ -0,0 +1,45 @@
+package medium;
+
+import common.*;
+
+public class AddTwoNumbers {
+    static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode c1 = l1;
+        ListNode c2 = l2;
+        ListNode s = new ListNode(0);
+        ListNode d = s;
+        int sum = 0;
+        while (c1 != null || c2 != null) {
+            sum /= 10;
+            if (c1 != null) {
+                sum += c1.val;
+                c1 = c1.next;
+            }
+            if (c2 != null) {
+                sum += c2.val;
+                c2 = c2.next;
+            }
+            d.next = new ListNode(sum % 10);
+            d = d.next;
+        }
+        if (sum /10 != 0)
+            d.next = new ListNode(sum / 10);
+        return s.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(2);
+        ListNode listNode2 = new ListNode(4);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(5);
+        ListNode listNode5 = new ListNode(6);
+        ListNode listNode6 = new ListNode(4);
+
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode4.next = listNode5;
+        listNode5.next = listNode6;
+
+        AddTwoNumbers.addTwoNumbers(listNode1, listNode4);
+    }
+}
