5785b06d8a1838983bb338aa06ebeee75ff619a2
Junyuan Zheng <joseph.zjy@gmail.com>
2017-08-26 22:08:42 -0700

    Add Question 121
    Add Question 121

---
+++ src/Question121BestTimeToBuyAndSellStock.java
@@ -0,0 +1,10 @@
+public class Question121BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        int max = 0, min = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < min) min = prices[i];
+            else if (prices[i] > min) max = Math.max(prices[i] - min, max);
+        }
+        return max;
+    }
+}
