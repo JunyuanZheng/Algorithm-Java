0f34fa8bc4d309939c837f7314b8c3404e7dce03
Junyuan Zheng <joseph.zjy@gmail.com>
2017-10-03 22:19:43 -0700

    Add Question 215
    Add Question 215

---
+++ src/Question215KthLargestElementInAnArray.java
@@ -0,0 +1,15 @@
+import java.util.PriorityQueue;
+
+public class Question215KthLargestElementInAnArray {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int val : nums) {
+            pq.offer(val);
+
+            if(pq.size() > k) {
+                pq.poll();
+            }
+        }
+        return pq.peek();
+    }
+}
