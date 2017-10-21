32e3369a56f9b9915a22050d27f5520de4ce4252
Junyuan Zheng <joseph.zjy@gmail.com>
2017-10-21 11:51:36 -0700

    Add Question 236
    Add Question 236

---
+++ src/Question236LowestCommonAncestorofaBinaryTree.java
@@ -0,0 +1,14 @@
+import common.TreeNode;
+
+import java.util.Stack;
+
+public class Question236LowestCommonAncestorofaBinaryTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        return left == null ? right : right == null ? left : root;
+    }
+}
