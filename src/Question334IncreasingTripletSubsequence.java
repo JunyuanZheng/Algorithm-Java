86fa2b98452b86f7fef702d0a5989401d79fa503
Junyuan Zheng <joseph.zjy@gmail.com>
2017-12-17 09:49:06 -0800

    Add Question 334
    Add Question 334

---
+++ src/Question334IncreasingTripletSubsequence.java
@@ -0,0 +1,14 @@
+public class Question334IncreasingTripletSubsequence {
+    public boolean increasingTriplet(int[] nums) {
+        int small = Integer.MAX_VALUE; int mid = Integer.MAX_VALUE;
+        for (int i : nums) {
+            if (i <= small)
+                small = i;
+            else if (i <= mid)
+                mid = i;
+            else
+                return true;
+        }
+        return false;
+    }
+}
