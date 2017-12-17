35c8b76317e0e35b8351e7d0a393fa2599030eca
Junyuan Zheng <joseph.zjy@gmail.com>
2017-12-17 13:32:13 -0800

    Add Question 331
    Add Question 331

---
+++ src/Question331VerifyPreorderSerializationofaBinaryTree.java
@@ -0,0 +1,14 @@
+public class Question331VerifyPreorderSerializationofaBinaryTree {
+    public boolean isValidSerialization(String preorder) {
+        String[] nodes = preorder.split(",");
+        int diff = 1;
+        for (String node : nodes) {
+            diff -= 1;
+            if (diff < 0)
+                return false;
+            if (!node.equals("#"))
+                diff += 2;
+        }
+        return diff == 0;
+    }
+}
