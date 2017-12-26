0c682a446ad5dae845e9940fe46b2dcffe09fff6
Junyuan Zheng <joseph.zjy@gmail.com>
2017-12-25 20:50:30 -0800

    Add Question 349
    Add Question 349

---
+++ src/Question349IntersectionofTwoArrays.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Question349IntersectionofTwoArrays {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> intersect = new HashSet<>();
+        for (int i : nums1) {
+            set.add(i);
+        }
+        for (int i : nums2) {
+            if (set.contains(i)) {
+                intersect.add(i);
+            }
+        }
+        int[] result = new int[intersect.size()];
+        int i = 0;
+        for (Integer num : intersect) {
+            result[i++] = num;
+        }
+        return result;
+    }
+}
